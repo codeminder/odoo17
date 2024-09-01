3fd2e756b4726bb2ef7f242435762429f0f2de37
Roman Rabotin <robcupertontan@gmail.com>
2024-09-01 15:45:45 +0300

    [IMP] hr_hospital: task_3 intermediate 8_1
    Changes to be committed:
    	modified:   hr_hospital/models/diagnosis.py
    	modified:   hr_hospital/models/doctor.py
    	modified:   hr_hospital/views/diagnosis_views.xml
    	modified:   hr_hospital/views/doctor_views.xml

--- hr_hospital/models/diagnosis.py
+++ hr_hospital/models/diagnosis.py
@@ -1,4 +1,7 @@
-from odoo import models, fields
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, fields
+from odoo.exceptions import ValidationError
 
 class Diagnosis(models.Model):
     _name = 'hr_hospital.diagnosis'
@@ -10,3 +13,26 @@ class Diagnosis(models.Model):
     patient_id = fields.Many2one('hr_hospital.patient', required=True)
     sick_id = fields.Many2one('hr_hospital.sick', required=True)
     treatment = fields.Text()
+
+    mentor_comment = fields.Text(string="Mentor Comment", help="Comment by the mentor doctor if the diagnosis is made by an intern.")
+
+    @api.onchange('doctor_id')
+    def _onchange_doctor_id(self):
+        """ Ensure that if the doctor is an intern, the mentor must be selected. """
+        if self.doctor_id and self.doctor_id.is_intern:  # Updated to `is_intern`
+            if not self.doctor_id.mentor_id:
+                raise ValidationError("Intern doctors must have a mentor assigned.")
+
+    @api.model
+    def create(self, vals):
+        diagnosis = super(Diagnosis, self).create(vals)
+        if diagnosis.doctor_id.is_intern and not diagnosis.mentor_comment:  # Updated to `is_intern`
+            raise ValidationError("A mentor's comment is required when an intern makes a diagnosis.")
+        return diagnosis
+
+    def write(self, vals):
+        res = super(Diagnosis, self).write(vals)
+        for record in self:
+            if record.doctor_id.is_intern and not record.mentor_comment:  # Updated to `is_intern`
+                raise ValidationError("A mentor's comment is required when an intern makes a diagnosis.")
+        return res
--- hr_hospital/models/doctor.py
+++ hr_hospital/models/doctor.py
@@ -10,6 +10,14 @@ class Doctor(models.Model):
 
     specialization = fields.Char()
     
+    is_intern = fields.Boolean(string="Intern") 
+    mentor_id = fields.Many2one(
+        'hr_hospital.doctor', 
+        string="Mentor Doctor", 
+        domain="[('is_intern', '=', False)]", 
+        help="The mentor doctor for this intern."
+    )
+    
     # Override name_get to call parent's name_get method
     def name_get(self):
         return super(Doctor, self).name_get()
