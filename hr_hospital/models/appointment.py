1229dd68c12dd2a1bec60c8e497589f0522225aa
Roman Rabotin <robcupertontan@gmail.com>
2024-09-02 20:58:58 +0300

    [IMP] hr_hospital: task_3-10
    Changes to be committed:
    	modified:   hr_hospital/models/appointment.py
    	modified:   hr_hospital/views/appointment_views.xml

--- hr_hospital/models/appointment.py
+++ hr_hospital/models/appointment.py
@@ -15,6 +15,20 @@ class Appointment(models.Model):
     appointment_date = fields.Datetime(required=True)
     sickness_id = fields.Many2one("hr_hospital.sick", string="Sickness")
     diagnosis_id = fields.Many2one('hr_hospital.diagnosis', string="Diagnosis")
+    appointment_occurred = fields.Boolean(default=False)
+    
+    # Restrict to duplicat appointment date for doctor
+    @api.constrains('appointment_date', 'doctor_id')
+    def _check_appointment_conflict(self):
+        """Ensure there are no conflicting appointments for the same doctor at the same time."""
+        for record in self:
+            conflicting_visit = self.search([
+                ('doctor_id', '=', record.doctor_id.id),
+                ('appointment_date', '=', record.appointment_date),
+                ('id', '!=', record.id)
+            ])
+            if conflicting_visit:
+                raise ValidationError('A doctor cannot have two appointments at the same time.')
     
     # Restrict to change appointment date for done visits
     @api.constrains('appointment_date', 'doctor_id')
