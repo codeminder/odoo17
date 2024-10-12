a84ff947623c76dbd6bcb089699f020e3eae4129
Roman Rabotin <robcupertontan@gmail.com>
2024-10-12 16:18:14 +0300

    [IMP] hr_hospital: task_4 part 12 final
    Changes to be committed:
    	modified:   hr_hospital/models/hr_hospital_analysis.py
    	modified:   hr_hospital/views/hr_hospital_analysis_views.xml

--- hr_hospital/models/hr_hospital_analysis.py
+++ hr_hospital/models/hr_hospital_analysis.py
@@ -9,6 +9,12 @@ class Analysis(models.Model):
     patient_id = fields.Many2one('hr_hospital.patient')
     doctor_id = fields.Many2one('hr_hospital.doctor')
     analysis_date = fields.Datetime()
+    sample_type = fields.Selection([
+        ('blood', 'Blood'),
+        ('urine', 'Urine'),
+        ('tissue', 'Tissue'),
+        ('other', 'Other')
+    ], required=True, default='blood')
     result = fields.Text()
     
     patient_first_name = fields.Char(related='patient_id.first_name', store=True)
